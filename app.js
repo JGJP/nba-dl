9c6a0ba08c38c318d13a32dc95a5003193c65f2d
J <j@jgarcia.jp>
2018-05-29 15:47:45 +0900

    add db

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const puppeteer = require("puppeteer");
 const { exec } = require("child_process");
-const fs = require('fs');
+const fs = require("fs");
+
+// DB
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+db.defaults({ downloaded: [] }).write();
 
 const DL_DIR = "./dl";
 
@@ -14,37 +21,42 @@ const DL_DIR = "./dl";
 	const LINK_SELECTOR = "#items ytd-grid-video-renderer:nth-child(INDEX) h3 a";
 	const LENGTH_SELECTOR = "#items ytd-grid-video-renderer";
 
-
+	// get list length
 	let listLength = await page.evaluate((sel) => {
 		return document.querySelectorAll(sel).length;
 	}, LENGTH_SELECTOR);
 
-	console.log(`videos found: ${listLength}`);
-
 	for(let i = 1; i <= listLength; i++){
 
-		let linkSelector = LINK_SELECTOR.replace("INDEX", i);
+		let linkSelector = LINK_SELECTOR.replace("INDEX", i),
+			link,
+			title;
 
 		// get title
-		let title = await page.evaluate((sel) => {
+		title = await page.evaluate((sel) => {
 			let el = document.querySelector(sel);
 			return el.innerHTML;
 		}, linkSelector);
 
-		// check title
-		if(!title.match(/Full Game Highlights \| Game 7/))
+		// cancel if not a target
+		if(!title.match(/Full Game Highlights/))
 			continue;
+
+		// get link
+		link = await page.evaluate((sel) => {
+			return document.querySelector(sel).getAttribute("href").replace(/^/, "https://youtube.com");
+		}, linkSelector)
 
 		// log valid title
 		console.log(title);
 
-		// get link
-		let link = await page.evaluate((sel) => {
-			return document.querySelector(sel).getAttribute("href").replace(/^/, "https://youtube.com");
-		}, linkSelector)
+		// cancel if previously downloaded
+		if(DBHas(link)){
+			continue;
+		}
 
 		// download
-		download(link);
+		download(link, title);
 
 	}
 
@@ -52,7 +64,13 @@ const DL_DIR = "./dl";
 
 })();
 
-function download(link){
+function DBHas(link){
+
+	return db.get("downloaded").find({ link: link }).value()
+
+}
+
+function download(link, title){
 
 	// check for dir
 	if (!fs.existsSync(DL_DIR)) fs.mkdirSync(DL_DIR);
@@ -62,11 +80,20 @@ function download(link){
 
 	// handle output
 	child.on("error", (code, signal) => {
-		console.log('child process exited with ' + `code ${code} and signal ${signal}`);
+		console.log("child process exited with " + `code ${code} and signal ${signal}`);
 	})
 
 	child.on("exit", (code, signal) => {
-		console.log('download complete');
+		console.log("download complete");
+		addToDB(link, title);
 	})
+
+}
+
+function addToDB(link, title){
+
+	db.get("downloaded")
+		.push({ link: link, title: title })
+		.write();
 
 }
